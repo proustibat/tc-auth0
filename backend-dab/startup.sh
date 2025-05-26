a39a2a5b116e2df56e0267525acba6b787d97f4c
proustibat <jennifer.proust@gmail.com>
2025-05-26 16:25:38 +0200

    start dab on azure

--- backend-dab/startup.sh
+++ backend-dab/startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-curl -L https://github.com/Azure/data-api-builder/releases/latest/download/Microsoft.DataApiBuilder.1.4.35.nupkg -o dab.nupkg
-
-dotnet tool install --add-source ./ --tool-path ./tools Microsoft.DataApiBuilder --version 1.4.35
-
-./tools/dab start
+chmod +x Azure.DataApiBuilder.Service
+./Azure.DataApiBuilder.Service --ConfigFile dab-config.json
