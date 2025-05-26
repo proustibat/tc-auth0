185c61a6560ccb09a4ade2928f13abc4fb78c0a3
proustibat <jennifer.proust@gmail.com>
2025-05-26 16:31:47 +0200

    rename start script

---
+++ backend-dab/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./Microsoft.DataApiBuilder --config dab-config.json --host.mode Development
--- backend-dab/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x Azure.DataApiBuilder.Service
-./Azure.DataApiBuilder.Service --ConfigFile dab-config.json
