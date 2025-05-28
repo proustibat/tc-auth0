954380ebc5595971fae9b2ff37f9b7f5e932c8d5
proustibat <jennifer.proust@gmail.com>
2025-05-28 14:41:49 +0200

    refactor(auth0provider): remove unnecessary wrapper

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,15 @@
+import { type AppState, Auth0Provider } from "@auth0/auth0-react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { lazy } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { AuthProviderWithNavigate } from "../auth/AuthProvider.tsx";
 import ErrorFallback from "../components/ErrorFallback.tsx";
 import Menu from "../components/Menu.tsx";
 import { ApolloWrapper } from "../graphql/ApolloWrapper.tsx";
+import { appRouter } from "../router.tsx";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 const TanStackRouterDevtools = import.meta.env.PROD
     ? () => null
@@ -22,13 +27,23 @@ export const Route = createRootRoute({
                 window.location.reload();
             }}
         >
-            <AuthProviderWithNavigate>
+            <Auth0Provider
+                domain={domain}
+                clientId={clientId}
+                authorizationParams={{
+                    redirect_uri: window.location.origin,
+                    audience,
+                }}
+                onRedirectCallback={(appState: AppState | undefined) => {
+                    appRouter.history.push(appState?.returnTo || window.location.pathname);
+                }}
+            >
                 <ApolloWrapper>
                     <Menu />
                     <Outlet />
                     <TanStackRouterDevtools />
                 </ApolloWrapper>
-            </AuthProviderWithNavigate>
+            </Auth0Provider>
         </ErrorBoundary>
     ),
 });
